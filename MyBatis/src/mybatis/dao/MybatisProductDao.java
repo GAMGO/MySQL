d36984de6a6a70da5362a818e0e6a253ddc89f54
GAMGO <gamgo445@gmail.com>
2025-08-06 15:48:08 +0900

    sql update 9-1

--- MyBatis/src/mybatis/dao/MybatisProductDao.java
+++ MyBatis/src/mybatis/dao/MybatisProductDao.java
@@ -1,9 +1,5 @@
 package mybatis.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
